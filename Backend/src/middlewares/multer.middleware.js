7682c2a1469328f877123281131956215dc687ed
Aryaswapnil07 <achintyasingh48@gmail.com>
2026-06-20 16:48:49 +0530

    fix: refactor multer middleware to ensure upload directory exists and improve file naming

--- Backend/src/middlewares/multer.middleware.js
+++ Backend/src/middlewares/multer.middleware.js
@@ -1,15 +1,25 @@
 import multer from "multer";
+import fs from "fs";
+
+const uploadPath = "./public/temp";
+
+// Create folder if it doesn't exist
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, {
+    recursive: true,
+  });
+}
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "./public/temp")
-    },
-    filename: function (req, file, cb) {
-      
-      cb(null, file.originalname)
-    }
-  })
-  
-export const upload = multer({ 
-    storage, 
-})
+  destination: function (req, file, cb) {
+    cb(null, uploadPath);
+  },
+
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+export const upload = multer({
+  storage,
+});
